059a94922abf6020d526415fe53d4667eca1a35f
s8189882 <s8189882@gmail.com>
2022-09-28 19:30:41 +0530

    Add UC3 - program parse CSV into POJO class for reading CSV Directly

--- OpenCSVandGsonLibraries/src/main/java/com/opencsv/csvreader/OpenCSVandGson/OpenCSVReader.java
+++ OpenCSVandGsonLibraries/src/main/java/com/opencsv/csvreader/OpenCSVandGson/OpenCSVReader.java
@@ -4,28 +4,28 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.Iterator;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
-import com.opencsv.exceptions.CsvValidationException;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 
 public class OpenCSVReader {
 	public static final String SAMPLE_CSV_FILE_PATH = "/Users/apoorvasanglikar/BridegeLabz/GitRepo/JavaP/JavaIO/OpenLibraries/OpenCSVandGsonLibraries/users.csv";
 	
-	public static void main(String[] args) throws IOException, CsvException  {
+	public static void main(String[] args) throws IOException {
 		try (
 			Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
-			CSVReader csvReader = new CSVReader(reader);
 		) {
-			List<String[]> records = csvReader.readAll();
-			for (String[] record : records) {
-				System.out.println("Name : " + record[0]);
-				System.out.println("Email : " + record[1]);
-				System.out.println("Phone : " + record[2]);
-				System.out.println("Country : " + record[3]);
-				System.out.println("================================");
-			}
+			CsvToBean<CSVUser> csvToBean = new CsvToBeanBuilder<CSVUser>(reader).withType(CSVUser.class).withIgnoreLeadingWhiteSpace(true).build();
+			Iterator<CSVUser> csvUserIterator = csvToBean.iterator();
+			while (csvUserIterator.hasNext()) {
+                CSVUser csvUser = csvUserIterator.next();
+                System.out.println("Name : " + csvUser.getName());
+                System.out.println("Email : " + csvUser.getEmail());
+                System.out.println("Phone : " + csvUser.getPhoneNumber());
+                System.out.println("Country : " + csvUser.getCountry());
+                System.out.println("==========================");
+            }
 		}
 	}
 }
