e2644c701cd56bfd7135a4cf262adf5782a61246
s8189882 <s8189882@gmail.com>
2022-09-28 19:13:57 +0530

    Add UC2 - Read All Records at Once

--- OpenCSVandGsonLibraries/src/main/java/com/opencsv/csvreader/OpenCSVandGson/OpenCSVReader.java
+++ OpenCSVandGsonLibraries/src/main/java/com/opencsv/csvreader/OpenCSVandGson/OpenCSVReader.java
@@ -4,27 +4,28 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import com.opencsv.exceptions.CsvValidationException;
 
 public class OpenCSVReader {
-	public static final String SAMPLE_CSV_FILE_PATH = "/Users/apoorvasanglikar/BridegeLabz/JavaPrograms/RPF-Java/eclipse-workspace/OpenCSVandGsonLibraries/users.txt";
+	public static final String SAMPLE_CSV_FILE_PATH = "/Users/apoorvasanglikar/BridegeLabz/GitRepo/JavaP/JavaIO/OpenLibraries/OpenCSVandGsonLibraries/users.csv";
 	
-	public static void main(String[] args) throws CsvValidationException, IOException  {
+	public static void main(String[] args) throws IOException, CsvException  {
 		try (
 			Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
 			CSVReader csvReader = new CSVReader(reader);
 		) {
-			String[] nextRecord;
-			while((nextRecord = csvReader.readNext()) != null) {
-				System.out.println("Name : " + nextRecord[0]);
-				System.out.println("Email : " + nextRecord[1]);
-				System.out.println("Phone : " + nextRecord[2]);
-				System.out.println("Country : " + nextRecord[3]);
+			List<String[]> records = csvReader.readAll();
+			for (String[] record : records) {
+				System.out.println("Name : " + record[0]);
+				System.out.println("Email : " + record[1]);
+				System.out.println("Phone : " + record[2]);
+				System.out.println("Country : " + record[3]);
 				System.out.println("================================");
 			}
 		}
 	}
-
 }
